76557b200e6d9c85acba494b82a22e748c9b578f
sisui0582 <1970749071@qq.com>
2020-05-21 20:43:13 +0800

    用户列表完成

---
+++ src/view/layout/user/user-add-or-pudate.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="userEdit">
+    <el-dialog center :visible.sync="dialogVisible" width="477px">
+      <div slot="title" class="title">{{ mode === "add" ? "新增用户" : "修改用户" }}</div>
+      <el-form ref="addFormRef" :model="addForm" :rules="rules" label-width="80px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" prop="phone">
+          <el-input v-model="addForm.phone"></el-input>
+        </el-form-item>
+        <el-form-item label="角色">
+          <el-select v-model="addForm.role_id" placeholder="请选择">
+            <el-option label="超级管理员" :value="1"></el-option>
+            <el-option label="管理员" :value="2"></el-option>
+            <el-option label="老师" :value="3"></el-option>
+            <el-option label="学生" :value="4"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="状态">
+          <el-select v-model="addForm.status" placeholder="请选择">
+            <el-option label="禁用" :value="0"></el-option>
+            <el-option label="启用" :value="1"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="备注" prop="remark">
+          <el-input v-model="addForm.remark"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submit">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialogVisible: false,
+      mode: "", //模式，add 代表新增  edit 代表修改
+      addForm: {
+        username: "", //用户名
+        email: "", //邮箱
+        phone: "", //手机号
+        role_id: "", //角色
+        status: "", //状态
+        remark: "" //备注
+      },
+      //校验规则
+      rules: {
+        //用户名
+        username: [
+          { required: true, message: "请输入用户名称", trigger: "blur" }
+        ],
+        email: [
+          {
+            required: true,
+            validator: (rule, value, callback) => {
+              if (!value) {
+                return callback(new Error("邮箱不能为空"));
+              }
+
+              //正侧
+              const reg = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+              if (!reg.test(value)) {
+                return callback(new Error("邮箱不符合规则"));
+              }
+              callback();
+            },
+            trigger: "blur"
+          }
+        ],
+        //手机号
+        phone: [
+          {
+            required: true,
+            validator: (rule, value, callback) => {
+              if (!value) {
+                return callback(new Error("电话不能为空"));
+              }
+              //手机正则
+              const reg = /^1[3456789][0-9]{9}$/;
+              if (!reg.test(value)) {
+                return callback(new Error("手机号不符合规则"));
+              }
+              callback();
+            },
+            trigger: "blur"
+          }
+        ],
+        //角色
+        role_id: [
+          { required: true, message: "请选择用户角色", trigger: "change" }
+        ],
+        //状态
+        status: [
+          { required: true, message: "请选择用户状态", trigger: "change" }
+        ],
+        //备注
+        remark: [{ required: true, message: "请写备注", trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    submit() {
+      this.$refs.addFormRef.validate(async valid => {
+        if (!valid) return;
+
+        let res = null;
+        if (this.mode === "add") {
+          res = await this.$axios.post("/user/add", this.addForm);
+          //console.log(res);
+        } else {
+          //修改
+        }
+
+        if (res.data.code === 200) {
+          this.$message({
+            message: this.mode === "add" ? "新增成功" : "编辑成功",
+            type: "success"
+          });
+
+          //关闭当前对话框
+          this.dialogVisible = false;
+
+          //刷新父组件的页面
+          this.$parent.search();
+        } else {
+          this.$message.error(res.data.message);
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.userEdit {
+  .title {
+    height: 53px;
+    text-align: center;
+    color: #fff;
+    background-color: #0e9cfa;
+    line-height: 53px;
+  }
+  .el-dialog__header {
+    padding: 0;
+  }
+  .el-dialog__headerbtn .el-dialog__close {
+    color: #fff;
+  }
+}
+</style>
--- src/view/layout/user/user.vue
+++ src/view/layout/user/user.vue
@@ -1,15 +1,170 @@
 <template>
   <div>
-      用户列表
+    <el-card shadow="always">
+      <el-form ref="searchFormRef" :model="searchForm" label-width="80px" inline>
+        <el-form-item label="用户名称" prop="username">
+          <el-input style="width:150px" v-model="searchForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="用户邮箱" prop="email">
+          <el-input style="width:150px" v-model="searchForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="角色" prop="role_id">
+          <el-select placeholder="请选择" v-model="searchForm.role_id" style="width:150px">
+            <el-option label="超级管理员" value="1"></el-option>
+            <el-option label="管理员" value="2"></el-option>
+            <el-option label="老师" value="3"></el-option>
+            <el-option label="学生" value="4"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="search" type="primary">搜索</el-button>
+          <el-button @click="creale" type="default">清除</el-button>
+          <el-button @click="add" type="primary">+新增用户</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <!-- 内容 -->
+    <el-card shadow="always" style="margin-top:20px">
+      <el-table :data="userList" style="width: 100%">
+        <el-table-column type="index" label="序号" width="80"></el-table-column>
+        <el-table-column prop="username" label="用户名"></el-table-column>
+        <el-table-column prop="phone" label="电话" ></el-table-column>
+        <el-table-column prop="email" label="邮箱" width="160"></el-table-column>
+        <el-table-column prop="role" label="角色"></el-table-column>
+        <el-table-column prop="remark" label="备注"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <span v-if="scope.row.status === 0" style="color:red">禁用</span>
+            <span v-if="scope.row.status === 1" style="color:#6ac144">启用</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="280">
+          <template slot-scope="scope">
+            <el-button type="primary">编辑</el-button>
+            <el-button
+              @click="changeStatus(scope.row.id)"
+              :type="scope.row.status === 0 ? 'success' : 'info'"
+            >{{scope.row.status === 0 ? "启用" : "禁用"}}</el-button>
+            <el-button @click="deleteUser(scope.row.id,scope.row.username)" type="default">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div style="margin-top:15px;text-align:center;">
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="page"
+          :page-sizes="[2, 3, 5, 10]"
+          :page-size="limit"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+        ></el-pagination>
+      </div>
+    </el-card>
+    <UserEdit ref="userEditRef"></UserEdit>
   </div>
 </template>
 
 <script>
+//引入 user-or-update.vue子组件
+import UserEdit from "./user-add-or-pudate";
 export default {
+  components: {
+    UserEdit
+  },
+  name: "user",
+  data() {
+    return {
+      searchForm: {
+        username: "", //用户名称
+        email: "", //邮箱
+        role_id: "" // 角色数字 1、超级管理员 2管理员、3老师、 4学生
+      },
+      page: 1, // 查询时候的页码
+      limit: 1, // 查询时候的页容量(每页查询多少条)
+      userList: [], // 展示用户列表所需要的数据
+      total: 0 // 总条数，分页时候用得着
+    };
+  },
+  created() {
+    this.getUserListData();
+  },
 
-}
+  methods: {
+    //搜索
+    search() {
+      this.page = 1;
+      this.getUserListData();
+    },
+    creale() {
+      this.$refs.searchFormRef.resetFields();
+      this.search();
+    },
+    async getUserListData() {
+      const res = await this.$axios.get("/user/list", {
+        params: {
+          page: this.page,
+          limit: this.limit,
+          ...this.searchForm
+        }
+      });
+      console.log(res);
+      if (res.data.code === 200) {
+        this.userList = res.data.data.items;
+        this.total = res.data.data.pagination.total;
+      }
+    },
+    handleSizeChange(val) {
+      //console.log(`每页 ${val} 条`);
+      this.limit = val;
+      this.search();
+    },
+    handleCurrentChange(val) {
+      //console.log(`当前页: ${val}`);
+      this.page = val;
+      this.getUserListData();
+    },
+    //更改当前状态
+    async changeStatus(id) {
+      const res = await this.$axios.post("/user/status", { id });
+      //console.log(res);
+      if (res.data.code === 200) {
+        this.$message({
+          message: "更改状态成功~",
+          type: "success"
+        });
+
+        //重新查询
+        this.search();
+      }
+    },
+    deleteUser(id, username) {
+      this.$confirm(`确定删除 ${username} 该用户吗？`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(async () => {
+          const res = await this.$axios.post("/user/remove", { id });
+          if (res.data.code === 200) {
+            this.$message({
+              message: "删除成功~",
+              type: "success"
+            });
+            // 重新查询
+            this.search();
+          }
+        })
+        .catch(() => {});
+    },
+    add() {
+      // 让新增用户的对话框显示出来
+      this.$refs.userEditRef.dialogVisible = true;
+      this.$refs.userEditRef.mode = "add";
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
